5640987320d61c3859887da981b65fd7bbc554c7
wexujk <carlospervas@gmail.com>
2021-03-11 16:38:31 -0600

    practica_0

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -13,13 +13,15 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "PEREZ_VASQUEZ"
+NAME = "PEREZ_VASQUEZ_CARLOS"
 
 def callback_laser_scan(msg):
     #
     # TODO:
     # Do something to detect if there is an obstacle in front of the robot.
     #
+	global obstacle
+	obstacle = msg.ranges[len(msg.ranges)//2] < 1.0
     return
 
 def main():
@@ -28,7 +30,8 @@ def main():
     rospy.Subscriber("/scan", LaserScan, callback_laser_scan)
     pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     loop = rospy.Rate(10)
-    
+    obstacle = False
+	cmd_vel = Twist()
     while not rospy.is_shutdown():
         #
         # TODO:
@@ -36,6 +39,9 @@ def main():
         # Move forward if there is no obstacle in front and stop otherwise.
         # Publish the message.
         #
+		cmd_vel.linear.x=0 if obstacle else 0.5
+		#cmd_vel.angular.z=0.5
+		pub_cmd_vel.publish(cmd_vel)
         loop.sleep()
 
 
