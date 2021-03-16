41389ef7ce33839e7995d8dd55e7f41fc94b2e24
Quimey Corona <quimeycoronamolina5mct1@gmail.com>
2021-03-16 14:55:47 -0600

    El robot avanza con velocidad lineal y angular

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -13,22 +13,28 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "CORONA MOLINA QUIMEY ALEJANDRO" 
 
 def callback_laser_scan(msg):
     #
     # TODO:
     # Do something to detect if there is an obstacle in front of the robot.
     #
+    #global obstacle
+    #obstacle = msg.ranges[len(msg.ranges)//2] < 1.0 #si la distancia que tienes en frente es menor a 1
+    print(msg.angle_min)
     return
 
 def main():
+    global obstacle    
     print "PRACTICE 00 - " + NAME
     rospy.init_node("practice00")
     rospy.Subscriber("/scan", LaserScan, callback_laser_scan)
     pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     loop = rospy.Rate(10)
     
+    obstacle = False    
+    cmd_vel = Twist()    
     while not rospy.is_shutdown():
         #
         # TODO:
@@ -36,6 +42,10 @@ def main():
         # Move forward if there is no obstacle in front and stop otherwise.
         # Publish the message.
         #
+        #cmd_vel.linear.x = 0 if obstacle else 0.5
+	cmd_vel.linear.x = 0.3
+        cmd_vel.angular.z= 0.5
+        pub_cmd_vel.publish(cmd_vel) 
         loop.sleep()
 
 
