a6f3181160e12c384b7871f5255653e6ab507772
CharlieML <charls_brian@hotmail.es>
2021-06-15 14:18:29 -0500

    Se ha agregado nombre a la practica 1

--- catkin_ws/src/students/scripts/practice01.py
+++ catkin_ws/src/students/scripts/practice01.py
@@ -18,7 +18,7 @@ from nav_msgs.msg import Path
 from nav_msgs.srv import *
 from collections import deque
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "MONTERRUBIO_LOPEZ"
 
 def dijkstra(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     #
