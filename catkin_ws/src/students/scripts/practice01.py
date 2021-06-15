a1043e30ff0ae87a485bb49c0b692292877c3043
daniela <d.c.carvajal29@gmail.com>
2021-06-14 23:44:24 -0500

    Practica 1 completada

--- catkin_ws/src/students/scripts/practice01.py
+++ catkin_ws/src/students/scripts/practice01.py
@@ -18,7 +18,7 @@ from nav_msgs.msg import Path
 from nav_msgs.srv import *
 from collections import deque
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "COLOHUA_CARVAJAL"
 
 def dijkstra(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     #
@@ -32,6 +32,44 @@ def dijkstra(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     # https://docs.python.org/2/library/heapq.html
     #
     
+    g_values = numpy.full(grid_map.shape, sys.maxint)  #numpy.full llena un arreglo
+    parent_nodes = numpy.full((grid_map.shape[0], grid_map.shape[1], 2), -1)
+    in_open_list = numpy.full(grid_map.shape, False)
+    in_closed_list = numpy.full(grid_map.shape, False)
+    steps = 0
+
+    open_list = []
+    heapq.heappush(open_list, (0, [start_r,start_c]))
+    g_values[start_r, start_c] = 0
+    in_open_list[start_r, start_c] = True
+    [r,c] = [start_r, start_c] #Nodo actual
+    
+    while len(open_list) > 0 and [r,c] != [goal_r, goal_c]:
+        [r,c] = heapq.heappop(open_list)[1] #Obtiene nodo
+        in_closed_list[r,c] = True #Se pasa a la lista cerrada
+        neighbors = [[r+1,c],[r-1,c],[r,c+1],[r,c-1]]
+        for [nr,nc] in neighbors:
+            if grid_map[nr,nc] != 0 or in_closed_list[nr,nc]: #Si nodo ocupado o en lista cerrada
+                continue
+            g = g_values[r,c] + 1 + cost_map[nr][nc]
+            
+            if g <  g_values[nr,nc]:
+                g_values[nr,nc] = g
+                parent_nodes[nr,nc] = [r,c]
+            if not in_open_list[nr,nc]:
+                in_open_list[nr,nc] = True
+                heapq.heappush(open_list, (g, [nr, nc]))
+            steps+=1
+
+    if [r,c] != [goal_r, goal_c]:
+	    print("Cannot calculate path by Dijsktra :'(")
+	    return []
+    path = []
+    while [parent_nodes[r,c][0], parent_nodes[r,c][1]] != [-1,-1]:
+	    path.insert(0, [r,c])
+	    [r,c] = parent_nodes[r,c]
+    print("Path calculated by Dijsktra " + str(steps) + " steps")
+    return path
 
 def a_star(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     #
@@ -46,6 +84,48 @@ def a_star(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     # https://docs.python.org/2/library/heapq.html
     #
     
+    g_values = numpy.full(grid_map.shape, sys.maxint)  #numpy.full llena un arreglo
+    f_values = numpy.full(grid_map.shape, sys.maxint)
+    parent_nodes = numpy.full((grid_map.shape[0], grid_map.shape[1], 2), -1)
+    in_open_list = numpy.full(grid_map.shape, False)
+    in_closed_list = numpy.full(grid_map.shape, False)
+    steps = 0
+
+    open_list = []
+    heapq.heappush(open_list, (0, [start_r,start_c]))
+    g_values[start_r, start_c] = 0
+    f_values[start_r, start_c] = 0
+    in_open_list[start_r, start_c] = True
+    [r,c] = [start_r, start_c] #Nodo actual
+    
+    while len(open_list) > 0 and [r,c] != [goal_r, goal_c]:
+        [r,c] = heapq.heappop(open_list)[1] #Obtiene nodo
+        in_closed_list[r,c] = True #Se pasa a la lista cerrada
+        neighbors = [[r+1,c],[r-1,c],[r,c+1],[r,c-1]]
+        for [nr,nc] in neighbors:
+            if grid_map[nr,nc] != 0 or in_closed_list[nr,nc]: #Si nodo ocupado o en lista cerrada
+                continue
+            g = g_values[r,c] + 1 + cost_map[nr][nc]  #Calcula el valor de g
+            h = numpy.abs(nr - goal_r) + numpy.abs(nc - goal_c)  #Calcula la heuristica
+            f = g + h #Calcula f
+
+            if g <  g_values[nr,nc]:
+                g_values[nr,nc] = g
+                f_values[nr,nc] = f
+                parent_nodes[nr,nc] = [r,c]
+            if not in_open_list[nr,nc]:
+                in_open_list[nr,nc] = True
+                heapq.heappush(open_list, (f, [nr, nc]))
+            steps+=1
+
+    if [r,c] != [goal_r, goal_c]:
+	    print("Cannot calculate path by A* :'(")
+	    return []
+    path = []
+    while [parent_nodes[r,c][0], parent_nodes[r,c][1]] != [-1,-1]:
+	    path.insert(0, [r,c])
+	    [r,c] = parent_nodes[r,c]
+    print("Path calculated by A* " + str(steps) + " steps")
 
 def get_maps():
     clt_static_map = rospy.ServiceProxy("/static_map"  , GetMap)
