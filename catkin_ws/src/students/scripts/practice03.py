f03c395401aaf0f1699ed8bf286d25e366372f00
fdolg <fdo.l.glez@gmail.com>
2021-06-21 15:23:20 -0500

    Entrega practica 3

--- catkin_ws/src/students/scripts/practice03.py
+++ catkin_ws/src/students/scripts/practice03.py
@@ -14,12 +14,13 @@ import numpy
 import heapq
 import rospy
 import copy
+import math
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Path
 from nav_msgs.srv import *
 from collections import deque
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "LOPEZ_GONZALEZ"
 
 def dijkstra(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     #
@@ -32,6 +33,46 @@ def dijkstra(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     # Documentation to implement priority queues in python can be found in
     # https://docs.python.org/2/library/heapq.html
     #
+    g_values        = numpy.full(grid_map.shape,sys.maxint) # Se inicializa la matriz de valores g
+    parent_nodes    = numpy.full((grid_map.shape[0],grid_map.shape[1],2),-1)
+    in_open_list    = numpy.full(grid_map.shape,False)
+    in_closed_list  = numpy.full(grid_map.shape,False)
+    steps = 0
+
+    open_list = []
+    heapq.heappush(open_list,(0,[start_r,start_c]))
+    g_values[start_r,start_c] =0
+    in_open_list[start_r, start_c] = True
+    [r,c] = [start_r, start_c]  
+    
+    while len(open_list)> 0 and [r,c] != [goal_r, goal_c]:
+        [r,c] = heapq.heappop(open_list)[1]
+        in_closed_list[r,c] = True
+        neighbors = [[r+1,c],[r-1,c],[r,c+1],[r,c-1]]
+        for [nr,nc] in neighbors: 
+            if grid_map[nr,nc] != 0 or in_closed_list[nr,nc]:
+                continue
+            g = g_values[r,c] + 1 + cost_map[nr][nc]
+
+            if g < g_values[nr,nc]:
+                g_values[nr,nc] =   g
+                parent_nodes[nr,nc] = [r,c]
+            if not in_open_list[nr,nc]:
+                in_open_list[nr,nc] = True
+                heapq.heappush(open_list,(g,[nr,nc]))
+            steps += 1  
+
+
+    if [r,c] != [goal_r,goal_c]:
+      print("Cannot calculate path by Dijsktra")
+      return[]
+    path=[]
+    while [parent_nodes[r,c][0],parent_nodes[r,c][1]] != [-1,-1]:
+        path.insert(0,[r,c])
+        [r,c] = parent_nodes[r,c]
+    print("Path calculated by Dijkstra after " + str(steps) + " steps")
+    return path
+
 
 def a_star(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     #
@@ -45,6 +86,51 @@ def a_star(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     # Documentation to implement priority queues in python can be found in
     # https://docs.python.org/2/library/heapq.html
     #
+    g_values        = numpy.full(grid_map.shape,sys.maxint)
+    f_values        = numpy.full(grid_map.shape,sys.maxint)  ######
+    parent_nodes    = numpy.full((grid_map.shape[0],grid_map.shape[1],2),-1)
+    in_open_list    = numpy.full(grid_map.shape,False)
+    in_closed_list  = numpy.full(grid_map.shape,False)
+    steps = 0
+
+    open_list =[]
+    heapq.heappush(open_list,(0,[start_r,start_c]))
+    g_values[start_r,start_c] = 0
+    f_values[start_r,start_c] = 0 
+    in_open_list[start_r, start_c] = True
+    [r,c] = [start_r, start_c]  
+    
+    while len(open_list)> 0 and [r,c] != [goal_r, goal_c]:
+        [r,c] = heapq.heappop(open_list)[1]
+        in_closed_list[r,c] = True
+        neighbors = [[r+1,c],[r-1,c],[r,c+1],[r,c-1]]
+        for [nr,nc] in neighbors: 
+            if grid_map[nr,nc] != 0 or in_closed_list[nr,nc]:
+                continue
+            g = g_values[r,c] + 1 + cost_map[nr][nc]
+            h = abs(goal_r-nr) + abs(goal_c-nc)
+            f = g + h
+            
+            if g < g_values[nr,nc]:
+                g_values[nr,nc] =   g
+                f_values[nr,nc] =   f 
+                parent_nodes[nr,nc] = [r,c]
+                           
+            if not in_open_list[nr,nc]:
+                in_open_list[nr,nc] = True
+                heapq.heappush(open_list,(f,[nr,nc]))
+            steps += 1  
+
+
+    if [r,c] != [goal_r,goal_c]:
+      print("Cannot calculate path by A-Star")
+      return[]
+    path = [] 
+    while [parent_nodes[r,c][0],parent_nodes[r,c][1]] != [-1,-1]:
+        path.insert(0,[r,c])
+        [r,c] = parent_nodes[r,c]
+    print("Path calculated by A-Star after " + str(steps) + " steps")
+    return path
 
 def get_smooth_path(original_path, alpha, beta):
     #
@@ -64,7 +150,44 @@ def get_smooth_path(original_path, alpha, beta):
     gradient     = [[0,0] for i in range(len(smooth_path))]# Gradient has N components of the form [x,y]. 
     epsilon      = 0.5                                     # This variable will weight the calculated gradient.
 
-    
+    print("Smoothing path with " + str(len(smooth_path)) + " points,using: " +str([alpha,beta]))
+    while gradient_mag > tolerance:
+
+        gradient_mag = 0   
+        [xi,yi] = smooth_path[0]
+        [xn,yn] = smooth_path[1]
+        [xo,yo] = original_path[0]
+        gx = alpha*(xi -xn) + beta*(xi-xo)
+        gy = alpha*(yi -yn) + beta*(yi-yo)
+        [xi,yi] = [xi-epsilon*gx, yi-epsilon*gy]
+        smooth_path[0] = [xi,yi]
+        gradient_mag += gx**2 + gy**2
+        
+        for i in range(1,len(smooth_path)-1):
+            [xi,yi] = smooth_path[i]
+            [xp,yp] = smooth_path[i-1]
+            [xn,yn] = smooth_path[i+1]
+            [xo,yo] = original_path[i]
+
+            gx = alpha*(2*xi - xp - xn) + beta*(xi-xo)
+            gy = alpha*(2*yi - yp - yn) + beta*(yi-yo)
+            [xi,yi] = [xi-epsilon*gx, yi-epsilon*gy]
+            smooth_path[i] = [xi,yi]
+            gradient_mag += gx**2 + gy**2
+        
+        [xi,yi] = smooth_path[-1]
+        [xp,yp] = smooth_path[-2]
+        [xo,yo] = original_path[-1]
+        
+        gx = alpha*(xn - xp) + beta*(xi - xo)
+        gy = alpha*(yn - yp) + beta*(yi - yo)
+        [xi,yi] = [xi-epsilon*gx, yi-epsilon*gy]
+        smooth_path[-1] = [xi,yi]
+        gradient_mag += gx**2 + gy**2
+        gradient_mag = math.sqrt(gradient_mag)
+
+        
+    print("Path smoothed succesfully(Y)")
     return smooth_path
 
 
