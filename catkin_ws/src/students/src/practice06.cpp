aced8306f804cc399ddcb02e0eacac5dfdd5994b
NormanBrandon <saldananormanbrandon1iv18@gmail.com>
2021-07-29 15:31:56 -0500

    practica 06 nombre, correccion

--- catkin_ws/src/students/src/practice06.cpp
+++ catkin_ws/src/students/src/practice06.cpp
@@ -17,7 +17,7 @@
 #include "geometry_msgs/Pose2D.h"
 #include "tf/transform_broadcaster.h"
 
-#define NOMBRE "APELLIDO_PATERNO_APELLIDO_MATERNO"
+#define NOMBRE "SALDANA_HERNANDEZ_NORMAN_BRANDON"
 
 #define LASER_DOWNSAMPLING  10
 #define SENSOR_NOISE        0.1
