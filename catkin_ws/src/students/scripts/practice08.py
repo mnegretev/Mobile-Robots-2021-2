9a3bbcce7ea26b991771d147a7898600f2d6a7ef
Luis Alvarez <soalalvarez@gmail.com>
2021-08-11 11:31:39 -0700

    Practica 8 resuelta

--- catkin_ws/src/students/scripts/practice08.py
+++ catkin_ws/src/students/scripts/practice08.py
@@ -8,6 +8,7 @@
 # given a colored point cloud using color segmentation.
 #
 
+import math
 import numpy
 import cv2
 import ros_numpy
@@ -15,7 +16,7 @@ import rospy
 from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import PointStamped
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "ALVAREZ_PEREZ"
 
 def segment_by_color(img_bgr, points):
     #
@@ -34,10 +35,29 @@ def segment_by_color(img_bgr, points):
     # [img_c, img_r] is the centroid of the segmented region in image coordinates.
     # [x,y,z] is the centroid of the segmented region in cartesian coordinate. 
     #
-    print(img_bgr[100, 300])
-    print(points[100,300])
-    return [100,100,0,0,0.3]
-    #return [img_c, img_r, x,y,z]
+    
+    img_hsv=cv2.cvtColor(img_bgr,cv2.COLOR_BGR2HSV)
+    img_bin=cv2.inRange(img_hsv,(28,230,127),(32,255,255))
+    indices=cv2.findNonZero(img_bin)
+    cv2.imshow("Binary",img_bin)
+    [img_x, img_y, a, b]=cv2.mean(indices)
+    print([img_x, img_y])
+    [x,y,z,counter]=[0,0,0,0]
+
+    for [[c,r]] in indices:
+        xt=points[r,c][0]
+        yt=points[r,c][1]
+        zt=points[r,c][2]
+        if math.isnan(xt) or math.isnan(yt) or math.isnan(zt):
+            continue
+        [x,y,z,counter]=[x+xt,y+yt,z+zt,counter+1]
+
+    x=x/counter if counter>0 else 0
+    y=y/counter if counter>0 else 0
+    z=z/counter if counter>0 else 0
+
+   
+    return [img_x, img_y, x,y,z]
 
 def callback_point_cloud(msg):
     global pub_point
