c44429be7f6cb1159f6c1441ab566a4233faa5bc
Gerardo Pastor <geralonso.unam@gmail.com>
2021-08-11 23:52:21 -0500

    Practica 8

--- catkin_ws/src/students/src/practice06.cpp
+++ catkin_ws/src/students/src/practice06.cpp
@@ -256,6 +256,7 @@ int main(int argc, char** argv)
              *
              * Move all particles a displacement given by delta_pose (Pose2D message) by calling the move_particles function.
              */
+            move_particles(particles, delta_pose.x, delta_pose.y, delta_pose.theta);
             pub_particles.publish(particles);
             map_to_odom_transform = get_map_to_odom_transform(robot_odom, get_robot_pose_estimation(particles));
         }
