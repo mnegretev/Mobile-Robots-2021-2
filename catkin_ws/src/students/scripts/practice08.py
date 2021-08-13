38c2490f392cf33edcc474b9340698a710d73b98
GeorgeO200 <george_orozco@comunidad.unam.mx>
2021-08-13 13:24:00 -0700

    Practica 8 Faltaba import math

--- catkin_ws/src/students/scripts/practice08.py
+++ catkin_ws/src/students/scripts/practice08.py
@@ -10,6 +10,7 @@
 
 import numpy
 import cv2
+import math
 import ros_numpy
 import rospy
 from sensor_msgs.msg import PointCloud2
@@ -63,38 +64,38 @@ def segment_by_color(img_bgr, points):
     #print(img_bgr[100, 300])
     #print(points[100,300])
     #return [100,100,0,0,0.3]
-    return [img_x, img_y, x,y,z]
+	return [img_x,img_y,x,y,z]
 
 def callback_point_cloud(msg):
-    global pub_point
-    arr = ros_numpy.point_cloud2.pointcloud2_to_array(msg)
-    rgb_arr = arr['rgb'].copy()
-    rgb_arr.dtype = numpy.uint32
-    r = numpy.asarray((rgb_arr >> 16) & 255, dtype=numpy.uint8)
-    g = numpy.asarray((rgb_arr >> 8) & 255, dtype=numpy.uint8)
-    b = numpy.asarray(rgb_arr & 255, dtype=numpy.uint8)
-    img_bgr = cv2.merge((b,g,r))
-    [centroid_x, centroid_y, x, y, z] = segment_by_color(img_bgr, arr)
-    p = PointStamped()
-    p.header.stamp = rospy.Time.now()
-    p.header.frame_id = "kinect_link"
-    [p.point.x, p.point.y, p.point.z] = [x,y,z]
-    pub_point.publish(p)
-    cv2.circle(img_bgr, (int(centroid_x), int(centroid_y)), 20, [0, 255, 0], thickness=3)
-    cv2.imshow("Color Segmentation", img_bgr)
-    cv2.waitKey(1)
+	global pub_point
+	arr = ros_numpy.point_cloud2.pointcloud2_to_array(msg)
+	rgb_arr = arr['rgb'].copy()
+	rgb_arr.dtype = numpy.uint32
+	r = numpy.asarray((rgb_arr >> 16) & 255, dtype=numpy.uint8)
+	g = numpy.asarray((rgb_arr >> 8) & 255, dtype=numpy.uint8)
+	b = numpy.asarray(rgb_arr & 255, dtype=numpy.uint8)
+	img_bgr = cv2.merge((b,g,r))
+	[centroid_x, centroid_y, x, y, z] = segment_by_color(img_bgr, arr)
+	p = PointStamped()
+	p.header.stamp = rospy.Time.now()
+	p.header.frame_id = "kinect_link"
+	[p.point.x, p.point.y, p.point.z] = [x,y,z]
+	pub_point.publish(p)
+	cv2.circle(img_bgr, (int(centroid_x), int(centroid_y)), 20, [0, 255, 0], thickness=3)
+	cv2.imshow("Color Segmentation", img_bgr)
+	cv2.waitKey(1)
 
 def main():
-    global pub_point
-    print "PRACTICE 08 - " + NAME
-    rospy.init_node("practice08")
-    rospy.Subscriber("/kinect/points", PointCloud2, callback_point_cloud)
-    pub_point = rospy.Publisher('/detected_object', PointStamped, queue_size=10)
-    rospy.spin()
+	global pub_point
+	print "PRACTICE 08 - " + NAME
+	rospy.init_node("practice08")
+	rospy.Subscriber("/kinect/points", PointCloud2, callback_point_cloud)
+	pub_point = rospy.Publisher('/detected_object', PointStamped, queue_size=10)
+	rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        main()
-    except rospy.ROSInterruptException:
-        pass
+	try:
+		main()
+	except rospy.ROSInterruptException:
+		pass
 
